68a6ea89b1b545f6bbdc0ad58f366e30f954aff6
Frank Hundley <mariner105@msn.com>
2021-11-27 17:43:05 -0800

    Added 05_arrays_slices

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	// Arrays
+	//var fruitArr [2]string
+
+	// Assign values
+	//fruitArr[0] = "Apple"
+	//fruitArr[1] = "Orange"
+
+	// Declare and assign
+	//fruitArr := [2]string{"Apple", "Orange"}
+
+	//fmt.Println(fruitArr)
+	//fmt.Println(fruitArr[1])
+
+	fruitSlice := []string{"Apple", "Orange", "Grape", "Cherry"}
+
+	fmt.Println(len(fruitSlice))
+	fmt.Println(fruitSlice[1:3])
 }

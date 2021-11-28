5024019211e49d90a736255607057d9560da64a5
Frank Hundley <mariner105@msn.com>
2021-11-27 18:14:03 -0800

    added maps and range

---
+++ 08_maps/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// define map[key]value
+	//emails := make(map[string]string)
+
+	// Assign key values
+	//emails["Bob"] = "bob@gmail.com"
+	//emails["Sharon"] = "sharon@gmail.com"
+	//emails["Mike"] = "mike@gmail.com"
+
+	// Declare map and add kv
+	emails := map[string]string{"Bob":"bob@gmail.com", "Sharon":"sharon@gmail.com"}
+
+	emails["Mike"] = "mike@gmail.com"
+
+	fmt.Println(emails)
+	fmt.Println(len(emails))
+	fmt.Println(emails["Bob"])
+
+	// Delete from map
+	delete(emails, "Bob")
+	fmt.Println(emails)
+
+}
